ab2ee4eedb7d7b9a997c3354f0e5fbafb19290e7
Farid Maulana <52988155+farid-maulana@users.noreply.github.com>
2022-06-03 21:44:12 +0700

    History purchases user

--- resources/views/clients/history_purchases.blade.php
+++ resources/views/clients/history_purchases.blade.php
@@ -6,12 +6,14 @@
     <div class="container">
         <div class="breadcrumb-banner d-flex flex-wrap justify-content-between">
             <div class="col-first">
-                <img class="img-fluid" src="{{ asset('images/logo-complete.png') }}" alt="logo" width="350px">
+                <img class="img-fluid" src="{{ asset('images/logo-complete.png') }}" alt="logo"
+                    width="350px">
                 <h1 class="mt-4">History Purchases</h1>
             </div>
             <div class="col-second">
                 <div class="banner-img">
-                    <img class="img-fluid" src="{{ asset('images/illustration.png') }}" alt="illustration" width="250px">
+                    <img class="img-fluid" src="{{ asset('images/illustration.png') }}"
+                        alt="illustration" width="250px">
                 </div>
             </div>
         </div>
@@ -20,8 +22,38 @@
 <!-- End Banner Area -->
 
 <div class="container my-5">
-  <div class="row">
-   
-  </div>
+    <div class="row">
+        <div class="col-md-12 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title mb-0">Top Courses</h4>
+                    <div class="table-responsive">
+                        <table class="table table-striped table-borderless">
+                            <thead>
+                                <tr>
+                                    <th>Date</th>
+                                    <th>Amount</th>
+                                    <th>Status</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($transactions as $transaction)
+                                    <tr>
+                                        <td>{{ date('d M Y', strtotime($transaction->created_at)); }}</td>
+                                        <td>{{ 'Rp ' . number_format($transaction->total, 0, ',', '.') }}</td>
+                                        <td>
+                                            <div class="{{ $transaction->status == 'pending' ? 'badge badge-warning' : ($transaction->status == 'completed' ? 'badge badge-success' : 'badge badge-danger') }} text-capitalize">{{ $transaction->status }}</div>
+                                        </td>
+                                        <td><a href="{{ route('history_purchases_detail', $transaction->id) }}">View Detail</a></td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
